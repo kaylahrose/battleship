9783a19d268249613087ace440d89d4600b6bef1
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-17 19:04:15 -0700

    Test: add render tests

--- lib/cell.rb
+++ lib/cell.rb
@@ -27,4 +27,14 @@ class Cell
         # require 'pry'; binding.pry
         ship.health -= 1
     end
+
+    def render
+        # returns a String representation of the Cell for when we need to print the board
+        #  A cell can potentially be rendered as:
+        
+        # ”.” if the cell has not been fired upon.
+        # “M” if the cell has been fired upon and it does not contain a ship (the shot was a miss).
+        # “H” if the cell has been fired upon and it contains a ship (the shot was a hit).
+        # “X” if the cell has been fired upon and its ship has been sunk.
+    end
 end
