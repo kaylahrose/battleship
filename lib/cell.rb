a6c048f3fb6052a234c436b0cd8391fb3eb93941
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-17 18:07:44 -0700

    Feat: Add Cell class

---
+++ lib/cell.rb
@@ -0,0 +1,13 @@
+class Cell
+    attr_reader :coordinate?
+    def initialize(coordinate)
+        @coordinate = coordinate
+       
+    end
+
+    def ship
+    end
+
+    def empty?
+    end
+end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,35 +3,40 @@ require './lib/cell'
 
 RSpec.describe Cell do
     # iteration 1
-    before(:each) do 
-        cell = Cell.new("B4")
-    end
+    # before(:each) do 
+    #     cell = Cell.new("B4")
+    # end
 
     describe '#initialize' do
         it 'exists' do
-            # cell = Cell.new("B4")
+            cell = Cell.new("B4")
+            # require 'pry'; binding.pry
             expect(cell).to be_an_instance_of(Cell)
         end
 
         it 'has a coordinate' do
-            # cell = Cell.new("B4")
+            cell = Cell.new("B4")
             expect(cell.coordinate).to eq("B4")
         end
             
         it 'has no ship' do
+            cell = Cell.new("B4")
             expect(cell.ship).to eq(nil)
         end
 
         it 'is empty by default' do
+            cell = Cell.new("B4")
             expect(cell.empty?).to eq(true)
         end
 
         it 'is an instance of the Ship class' do
+            cell = Cell.new("B4")
             cruiser = Ship.new("Cruiser", 3)
             expect(cruiser).to be_an_instance_of(Ship)
         end
 
         it 'places ship' do
+            cell = Cell.new("B4")
             cruiser = Ship.new("Cruiser", 3)
             cell.place_ship(cruiser)
             cell.ship
@@ -39,6 +44,7 @@ RSpec.describe Cell do
         end
 
         it 'returns if empty' do
+            cell = Cell.new("B4")
             cruiser = Ship.new("Cruiser", 3)
             cell.place_ship(cruiser)
             cell.ship
