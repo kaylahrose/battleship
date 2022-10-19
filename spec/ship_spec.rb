1c639118bcbc9e5becf51c259be6faf155cf1810
Aether Kerstens <philippekerstens@gmail.com>
2022-10-19 14:22:43 -0700

    Refactor an before each do into ship spec

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,47 +2,44 @@ require './lib/ship'
 
 RSpec.describe Ship do
   # iteration 1
+  before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
   describe '#initialize' do
     it 'exists' do
-      cruiser = Ship.new("Cruiser", 3)
-      expect(cruiser).to be_an_instance_of(Ship)
+      expect(@cruiser).to be_an_instance_of(Ship)
     end
-    
+
     it 'has a name' do
-      cruiser = Ship.new("Cruiser", 3)
-      expect(cruiser.name).to eq("Cruiser")
+      expect(@cruiser.name).to eq("Cruiser")
     end
 
     it 'has a length' do
-      cruiser = Ship.new("Cruiser", 3)
-      expect(cruiser.length).to eq(3)
+      expect(@cruiser.length).to eq(3)
     end
 
     it 'has health' do
-      cruiser = Ship.new("Cruiser", 3)
-      expect(cruiser.health).to eq(3)
+      expect(@cruiser.health).to eq(3)
     end
 
     it 'can float' do
-      cruiser = Ship.new("Cruiser", 3)
-      expect(cruiser.sunk?).to eq(false)
+      expect(@cruiser.sunk?).to eq(false)
     end
 
     it 'can be hit' do
-      cruiser = Ship.new("Cruiser", 3)
-      cruiser.hit
-      expect(cruiser.health).to eq(2)
+      @cruiser.hit
+      expect(@cruiser.health).to eq(2)
     end
 
     it 'can be sunk' do
-      cruiser = Ship.new("Cruiser", 3)
-      cruiser.hit
-      expect(cruiser.health).to eq(2)
-      cruiser.hit
-      expect(cruiser.health).to eq(1)
-      expect(cruiser.sunk?).to eq(false)
-      cruiser.hit
-      expect(cruiser.sunk?).to eq(true)
+      @cruiser.hit
+      expect(@cruiser.health).to eq(2)
+      @cruiser.hit
+      expect(@cruiser.health).to eq(1)
+      expect(@cruiser.sunk?).to eq(false)
+      @cruiser.hit
+      expect(@cruiser.sunk?).to eq(true)
     end
   end
 end
