9aa4d2344bc84706ae029808435bd9b47600d2c6
Aether Kerstens <philippekerstens@gmail.com>
2022-10-20 15:51:22 -0700

    Test: add shuffled coordinates to break valid_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -53,6 +53,7 @@ RSpec.describe Board do
     board.valid_placement?(cruiser, ["A1", "A2", "A4"])
     # require 'pry'; binding.pry
 
+
     expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
     expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
@@ -75,6 +76,7 @@ RSpec.describe Board do
 
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
     expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+    expect(board.valid_placement?(cruiser, ["A1", "A4", "A3"])).to eq(false)
   end
 
 end
