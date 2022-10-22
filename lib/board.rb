0cf619a8c29de486eb599867f183e56ce6901228
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-21 17:13:01 -0700

    Refactor: board methods to improve readability

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,6 @@ class Board
   attr_accessor :cells
 
   def initialize
-    # require 'pry'; binding.pry
     @cells = {
       'A1' => Cell.new('A1'),
       'A2' => Cell.new('A2'),
@@ -26,66 +25,63 @@ class Board
 
   def valid_coordinate?(coordinate)
     cells.keys.any? do |key_coordinate|
-      # require 'pry'; binding.pry
       key_coordinate == coordinate
     end
-    # require 'pry'; binding.pry
-  end
-
-  def check_overlap
-    # check if any of the cell
-    if cell.ship == ship
-      true
-    else
-      # call invalid method
-      false
-    end
-      # ?check if any of the coordinates/cells have coordinates already
-
   end
 
   def valid_placement?(ship, coordinate_array)
-    # if check_overlap == true
-    # access ship == nil
-    # call ship inside cell, then
+    if overlap?(coordinate_array) == false
+      valid_placement_array_length?(ship, coordinate_array)
+    else
+      false
+    end
+  end
 
+  def overlap?(coordinate_array)
+    coordinate_array.any? do |coordinate|
+      cells[coordinate].ship_present?
+    end
+  end
 
-    require 'pry'; binding.pry
-    # have a conditional statement
-    # check if the coordinate are full before correct length verification
+  def valid_placement_array_length?(ship, coordinate_array)
     if coordinate_array.length == ship.length
-      # iterate through coordinates and shovel letter into one array and numbers into number array
+      consecutive_coordinates?(coordinate_array)
+    else
+      false
+    end
+  end
 
-      letter = []
-      number = []
-      coordinate_array.each do |coordinate|
-        letter << coordinate[0]
-        number << coordinate[1]
-      end
-      # if the letters in the array are all the same
-      # require 'pry'; binding.pry
-      # letter.sort!
-      # number.sort!
+  def consecutive_coordinates?(coordinate_array)
+    separate_coordinates(coordinate_array)
+    if @@letter.uniq.size <= 1
+      consec_coord_num?
+    elsif @@number.uniq.size <= 1
+      consec_coord_letter?
+    else
+      false
+    end
+  end
 
-      if letter.uniq.size <= 1
-        # check if the numbers count in order
-        if number == number.sort
-          number[-1].to_i == number[0].to_i + (number.length - 1)
-        else
-          false
-        end
-      # if the numbers in the array are all the same
-      elsif number.uniq.size <= 1
-        # check if the letters go in order
-        # require 'pry'; binding.pry
-        if letter == letter.sort
-          letter[-1].ord == letter[0].ord + (letter.length - 1)
-        else
-          false
-        end
-      else
-        false
-      end
+  def separate_coordinates(coordinate_array)
+    @@letter = []
+    @@number = []
+    coordinate_array.each do |coordinate|
+      @@letter << coordinate[0]
+      @@number << coordinate[1]
+    end
+  end
+
+  def consec_coord_num?
+    if @@number == @@number.sort
+      @@number[-1].to_i == @@number[0].to_i + (@@number.length - 1)
+    else
+      false
+    end
+  end
+
+  def consec_coord_letter?
+    if @@letter == @@letter.sort
+      @@letter[-1].ord == @@letter[0].ord + (@@letter.length - 1)
     else
       false
     end
@@ -100,6 +96,5 @@ class Board
       false
       # puts "Invalid placement, please try again"
     end
-    # require 'pry'; binding.pry
   end
 end
